d00ed51adbaf71e6e4dfb5c5a979296d0a8a644c
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-10-04 19:21:44 +0200

    fix: add try catch

--- src/main/handlers/initHandlers/puppeteerBrowser_installation.ts
+++ src/main/handlers/initHandlers/puppeteerBrowser_installation.ts
@@ -2,7 +2,6 @@ import { IpcMainEvent, IpcMainInvokeEvent } from 'electron'
 
 import { ConfigData } from '../../../class/ConfigData'
 import { channels } from '../../../shared/constants'
-import { exec } from 'child_process'
 import { getMainLog } from '../../main'
 import { getMainWindow } from '../../memory'
 import { installMandatoryBrowser } from 'lighthouse-plugin-ecoindex/install-browser.cjs'
@@ -19,44 +18,11 @@ export const initPuppeteerBrowserInstallation = async (
     const mainLog = getMainLog().scope(
         'main/initialization/initPuppeteerBrowserInstallation'
     )
-    const toReturned = new ConfigData('puppeteer_browser_installation')
-    const browserInstalled = await installMandatoryBrowser()
-    return new Promise<ConfigData>((resolve) => {
-        if (browserInstalled) {
-            toReturned.result = true
-            toReturned.message = `puppeteer and browsers are installed`
-            getMainWindow().webContents.send(
-                channels.HOST_INFORMATIONS_BACK,
-                toReturned
-            )
-            return resolve(toReturned)
-        } else {
-            toReturned.error = `puppeteer and browsers can't be installed`
-            toReturned.message = `puppeteer and browsers can't be installed`
-            return resolve(toReturned)
-        }
-        const cmds = [
-            `npm install -g puppeteer --loglevel=error`,
-            // `npm install -g puppeteer@21.9.0 --loglevel=error`,
-            // Mandatory for testing browser installed
-            `npx puppeteer browsers install chrome`,
-            `npx puppeteer browsers install chrome-headless-shell`,
-            // Mandatory for lighthouse-plugin-ecoindex
-            `npx puppeteer browsers install chrome@121.0.6167.85`,
-            `npx puppeteer browsers install chrome-headless-shell@121.0.6167.85`,
-        ]
-        const cmd = cmds.join(` && `)
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                mainLog.error(`exec error: ${error}`)
-                toReturned.error = error
-                toReturned.message = `puppeteer and browsers can't be installed`
-                return resolve(toReturned)
-            }
-            if (stderr) mainLog.debug(`stderr: ${stderr}`)
-            if (stdout) {
-                const returned: string = stdout.trim()
-                mainLog.debug(`log: ${returned}`)
+    try {
+        const toReturned = new ConfigData('puppeteer_browser_installation')
+        const browserInstalled = await installMandatoryBrowser()
+        return new Promise<ConfigData>((resolve) => {
+            if (browserInstalled) {
                 toReturned.result = true
                 toReturned.message = `puppeteer and browsers are installed`
                 getMainWindow().webContents.send(
@@ -64,7 +30,14 @@ export const initPuppeteerBrowserInstallation = async (
                     toReturned
                 )
                 return resolve(toReturned)
+            } else {
+                toReturned.error = `puppeteer and browsers can't be installed`
+                toReturned.message = `puppeteer and browsers can't be installed`
+
+                return resolve(toReturned)
             }
         })
-    })
+    } catch (error) {
+        mainLog.error(`Error on initPuppeteerBrowserInstallation 🚫`, error)
+    }
 }
