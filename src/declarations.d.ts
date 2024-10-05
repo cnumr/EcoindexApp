0db8268376decba0217639e432d7ba4bb36cb820
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-10-05 09:43:50 +0200

    feat: to prepare next version of collect

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,6 +7,15 @@ declare module 'src/shared/tailwind-helper' {
 }
 declare module '*.md'
 declare module 'lighthouse-plugin-ecoindex/install-browser.cjs'
+declare module 'lighthouse-plugin-ecoindex/run.cjs' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    export default function runCourses(cliFlags: any): Promise<void>
+}
+declare module 'lighthouse-plugin-ecoindex/cli/index.mjs' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    export default function begin(): Promise<void>
+}
+// declare module '../../../node_modules/lighthouse-plugin-ecoindex/run.cjs'
 
 // declare module '*.css';
 // declare module '*.png';
